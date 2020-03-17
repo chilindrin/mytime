9144de9b1ddc2541fb93205b9c5b281d7ed7553d
gua <anguerrero@gmail.com>
2020-03-17 22:01:23 +0100

    Generate report with current info

--- app/src/main/java/com/chilin/org/CheckActivity.java
+++ app/src/main/java/com/chilin/org/CheckActivity.java
@@ -1,14 +1,16 @@
 package com.chilin.org;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.Gravity;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
-import com.chilin.org.db.TimeDBDealer;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.chilin.org.db.TimeRegister;
+
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -17,26 +19,48 @@ public class CheckActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TimeRegister timeRegister = new TimeRegister();
+        TableLayout tableLayout = createTable();
+        setContentView(tableLayout);
+    }
 
+    private TableLayout createTable() {
+        TimeRegister timeRegister = new TimeRegister();
         TableLayout tableLayout = new TableLayout(this);
         List<String[]> dbResults = timeRegister.getAllDataInDB(tableLayout.getContext());
-        for (int i = 0; i < 10; i++){
-            TableRow tableRow = new TableRow(this);
-            Button button = new Button(this);
-            button.setText("1");
-            tableRow.addView(button);
-
-            button = new Button(this);
-            button.setText("2");
-            tableRow.addView(button);
-
-            button = new Button(this);
-            button.setText("3");
-            tableRow.addView(button);
-
+        for (String[] dbResult:dbResults) {
+            TableRow tableRow = createRow(dbResult);
             tableLayout.addView(tableRow);
         }
-        setContentView(tableLayout);
+        return tableLayout;
+    }
+
+    private TableRow createRow(String[] dbResult) {
+        TextView dateText = createTextCell(dbResult[0]);
+        TextView comingText = createTextCell(dbResult[1]);
+        TextView leavingText = createTextCell(dbResult[2]);
+        TextView separatorText1 = createTextCell("||");
+        TextView separatorText2 = createTextCell("||");
+        TextView separatorText3 = createTextCell("||");
+        TextView separatorText4 = createTextCell("||");
+
+        TableRow tableRow = new TableRow(this);
+        tableRow.addView(separatorText1);
+        tableRow.addView(dateText);
+        tableRow.addView(separatorText2);
+        tableRow.addView(comingText);
+        tableRow.addView(separatorText3);
+        tableRow.addView(leavingText);
+        tableRow.addView(separatorText4);
+        return tableRow;
+    }
+
+    private TextView createTextCell(String text){
+        if (StringUtils.isBlank(text)){
+            text = "<not yet inserted>";
+        }
+        TextView textCell = new TextView(this);
+        textCell.setText(text);
+        textCell.setGravity(Gravity.CENTER);
+        return textCell;
     }
 }
